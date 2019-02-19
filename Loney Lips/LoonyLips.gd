8c5388718399b92874b9f59986fb103777ca8b0a
hujal54 <hujal54@gmail.com>
2019-02-19 21:45:44 +0100

    Ending the Game

--- Loney Lips/LoonyLips.gd
+++ Loney Lips/LoonyLips.gd
@@ -13,9 +13,13 @@ func _ready():
 func _welcome():
 	$Blackboard/StoryText.text =("Hi Player. Welcome to Loony Lips. \n Today we're gonna tell a story. \n Press Ok to continue")
 	
+	
 func _on_TextureButton_pressed():
-	var new_text = $Blackboard/TextBox.get_text ()
-	_on_TextBox_text_entered(new_text)
+	if is_story_done():
+		get_tree().reload_current_scene()
+	else:
+		var new_text = $Blackboard/TextBox.get_text ()
+		_on_TextBox_text_entered(new_text)
 
 	
 func _on_TextBox_text_entered(new_text):
@@ -23,15 +27,22 @@ func _on_TextBox_text_entered(new_text):
 	$Blackboard/TextBox.text = ""
 	check_player_word_length()
 	
+func is_story_done():
+	return player_words.size() == prompt.size()
+	
 func prompt_player():
 	$Blackboard/StoryText.text = ("Can I have " + prompt[player_words.size()] + ", please ?")
 	
 func check_player_word_length():
-	if player_words.size() == prompt.size():
+	if is_story_done():
 		tell_story()
 	else:
 		prompt_player()
 		
-		
 func tell_story ():
 	$Blackboard/StoryText.text = story % player_words
+	end_game()
+
+func end_game():
+	$Blackboard/TextBox.queue_free()
+	$Blackboard/TextureButton/RichTextLabel.text= "Again!"
